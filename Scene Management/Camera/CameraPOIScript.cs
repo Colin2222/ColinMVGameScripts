de9fcaddc208dfb1c6973af9f164bff56e7525e2
Colin2222 <crlew@outlook.com>
2022-05-20 17:51:34 -0500

    Camera rework
    Camera can now be changed to follow other than the player by activating/deactivating virtual cinemachine cameras. Camera points of interest will activate higher-priority vcams upon trigger entry.

---
+++ Scene Management/Camera/CameraPOIScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPOIScript : MonoBehaviour
+{
+    public GameObject vcam;
+
+    void OnTriggerEnter2D(Collider2D other){
+        vcam.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        vcam.SetActive(false);
+    }
+}
--- Scene Management/Camera/CameraScript.cs
+++ Scene Management/Camera/CameraScript.cs
@@ -8,15 +8,21 @@ public class CameraScript : MonoBehaviour
     [System.NonSerialized]
     public Transform followTransform;
     public float verticalOffset;
+    public Camera camera;
+    //public CinemachineVirtualCamera cCamera;
 
     void Start()
     {
-        followTransform = GameObject.FindWithTag("PlayerTag").transform;
+        ResetFollowTransform();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y + verticalOffset, this.transform.position.z);
+    }
+
+    public void ResetFollowTransform(){
+        followTransform = GameObject.FindWithTag("PlayerTag").transform;
     }
 }
